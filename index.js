876ea21e7e783678bc0e485b0ba7b67eb8aeb741
vkrutsina <vera.krutsina@gmail.com>
2020-08-28 11:05:48 -0700

    snake movement functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const score = document.getElementById('score');
 let squares = [];
 let currSnake = [2, 1, 0];
 let direction = 1;
+let width = 10;
 
 function createGrid() {
   //create 100 of these elements with a for loop
@@ -29,3 +30,26 @@ move();
 
 const timerId = setInterval(move, 1000);
 // clearInterval(timerId);
+
+// 39 = right
+// 38 = up
+// 37 = left
+// 40 = down
+
+function control(e) {
+  if (e.keyCode === 39) {
+    console.log('right');
+    direction = 1;
+  } else if (e.keyCode === 38) {
+    console.log('up');
+    direction = -width;
+  } else if (e.keyCode === 37) {
+    console.log('left');
+    direction = -1;
+  } else if (e.keyCode === 40) {
+    console.log('down');
+    direction = +width;
+  }
+}
+
+document.addEventListener('keyup', control);
