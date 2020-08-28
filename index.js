f477679dc1b1991aa6119a01a33603effa783a2b
vkrutsina <vera.krutsina@gmail.com>
2020-08-28 11:24:53 -0700

    game loss when hit wall logic

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+/* eslint-disable complexity */
 const grid = document.querySelector('.grid');
 const startBtn = document.getElementById('start');
 const score = document.getElementById('score');
 let squares = [];
 let currSnake = [2, 1, 0];
 let direction = 1;
-let width = 10;
+const width = 10;
 
 function createGrid() {
   //create 100 of these elements with a for loop
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < width * width; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     grid.appendChild(square);
@@ -20,6 +21,18 @@ createGrid();
 currSnake.forEach((i) => squares[i].classList.add('snake'));
 
 function move() {
+  //if snake has hit bottom, right wall, left wall, top
+
+  if (
+    (currSnake[0] + width >= width * width && direction === width) ||
+    (currSnake[0] % width === width - 1 && direction === 1) ||
+    (currSnake[0] % width === 0 && direction === -1) ||
+    (currSnake[0] - width < 0 && direciton === -width) ||
+    squares[currSnake[0] + direction].classList.contains('snake')
+  ) {
+    return clearInterval(timerId);
+  }
+
   const tail = currSnake.pop();
   squares[tail].classList.remove('snake');
   currSnake.unshift(currSnake[0] + direction);
@@ -28,7 +41,7 @@ function move() {
 
 move();
 
-const timerId = setInterval(move, 1000);
+let timerId = setInterval(move, 1000);
 // clearInterval(timerId);
 
 // 39 = right
